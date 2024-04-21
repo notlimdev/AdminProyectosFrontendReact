54bd31a38731fff345bdce5250d26d1644cc2973
Milton Raúl Vergara García <87380726+MilVG@users.noreply.github.com>
2024-04-21 00:33:25 -0500

    new cambio en confirmar cuenta

--- src/pages/ConfirmarCuenta.jsx
+++ src/pages/ConfirmarCuenta.jsx
@@ -41,8 +41,9 @@ const ConfirmarCuenta = () => {
         <span className='text-slate-700'>Proyetos</span>
       </h1>
 
-      <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+      <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-blue-500'>
         {msg && <Alerta alerta={alerta} />}
+
         {cuentaConfirmada && (
           <Link
           className='block text-center my-5 text-slate-500 uppercase text-sm'
